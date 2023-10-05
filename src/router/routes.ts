8fd5ce0e326672f2191aa05a002c35bf7a3d03ad
Mr.Chen <2490197735@qq.com>
2023-10-05 17:33:16 +0800

    添加了管理题目路由和更新题目路由，以及创建题目路由

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -7,6 +7,7 @@ import UserLayout from "@/layouts/UserLayout.vue";
 import UserLoginView from "@/views/user/UserLoginView.vue";
 import UserRegisterView from "@/views/user/UserRegisterView.vue";
 import AddQuestionView from "@/views/question/AddQuestionView.vue";
+import ManageQuestionView from "@/views/question/MangeQuestionView.vue";
 
 export const routes: Array<RouteRecordRaw> = [
   {
@@ -33,9 +34,25 @@ export const routes: Array<RouteRecordRaw> = [
     path: "/add/question",
     name: "创建题目",
     component: AddQuestionView,
-    meta: {
-      access: ACCESS_ENUM.ADMIN,
-    },
+    // meta: {
+    //   access: ACCESS_ENUM.ADMIN,
+    // },
+  },
+  {
+    path: "/update/question",
+    name: "更新题目",
+    component: AddQuestionView,
+    // meta: {
+    //   access: ACCESS_ENUM.ADMIN,
+    // },
+  },
+  {
+    path: "/mange/question",
+    name: "管理题目",
+    component: ManageQuestionView,
+    // meta: {
+    //   access: ACCESS_ENUM.ADMIN,
+    // },
   },
   {
     path: "/hide",
